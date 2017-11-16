cf3241b0e1b77274b92f70c3d63f60a3d724e27a
dllo <13614107915@163.com>
2017-11-16 19:58:31 +0800

    111

---
+++ src/main/java/com/lanou/mapper/SysMenuMapper.java
@@ -0,0 +1,22 @@
+package com.lanou.mapper;
+
+import com.lanou.bean.SysMenu;
+
+import java.util.List;
+
+public interface SysMenuMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysMenu record);
+
+    int insertSelective(SysMenu record);
+
+    SysMenu selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysMenu record);
+
+    int updateByPrimaryKey(SysMenu record);
+
+    //查询全部
+    List<SysMenu> findAllMenu();
+}
